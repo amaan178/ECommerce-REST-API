75e4e93e4e97692e9c49b2f003e73a0fa5b4be6e
Amaan Jambura <amaanjambura786@gmail.com>
2021-08-08 22:43:11 +0530

    added method to report multiple errors foe exception handling

--- app/Traits/ResponseHelper.php
+++ app/Traits/ResponseHelper.php
@@ -12,6 +12,11 @@ trait ResponseHelper
         return response()->json($data, $code);
     }
 
+    protected function reportMultipleErrors(mixed $message, int $code)
+    {
+        return response()->json(['error' => $message, 'code' =>$code], $code);
+    }
+
     protected function errorResponse(string $message, int $code)
     {
         return response()->json(['error' => $message, 'code' =>$code], $code);
