26a8651bdf2f6b637a14e850973a4238cc63378b
Amaan Jambura <amaanjambura786@gmail.com>
2021-08-09 21:34:36 +0530

    added resourceful routes for additional relationships

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\BuyerCategoryController;
+use App\Http\Controllers\BuyerProductController;
 use App\Http\Controllers\BuyersController;
+use App\Http\Controllers\BuyerSellerController;
+use App\Http\Controllers\BuyerTransactionController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\SellersController;
+use App\Http\Controllers\TransactinCategoryController;
 use App\Http\Controllers\TransactionsController;
+use App\Http\Controllers\TransactionSellerController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -27,3 +33,10 @@ Route::resource('sellers', SellersController::class)->only('index', 'show');
 Route::resource('users', UsersController::class)->except('create,edit');
 Route::resource('products', ProductsController::class)->only('index', 'show');
 Route::resource('transactions', TransactionsController::class)->only('index', 'show');
+
+Route::resource('transactions.categories', TransactinCategoryController::class)->only('index');
+Route::resource('transactions.sellers', TransactionSellerController::class)->only('index');
+Route::resource('buyers.transactions', BuyerTransactionController::class)->only('index');
+Route::resource('buyers.products', BuyerProductController::class)->only('index');
+Route::resource('buyers.sellers', BuyerSellerController::class)->only('index');
+Route::resource('buyers.categories', BuyerCategoryController::class)->only('index');
