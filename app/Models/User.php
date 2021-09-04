ac67e62b5788f31473c2ad95e20172d48431d0a4
Amaan Jambura <amaanjambura786@gmail.com>
2021-09-04 19:51:41 +0530

    updated user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,9 +27,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'email_verified_at',
         'verified',
-        'verification_token',
         'admin'
     ];
 
