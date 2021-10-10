89c8fd74666039fee445ce07188413b2eb898eea
Amaan Jambura <amaanjambura786@gmail.com>
2021-10-10 17:02:19 +0530

    added BuyerPolicy and added it to required relationship's middlewares to the methods

---
+++ app/Policies/BuyerPolicy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Buyer;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class BuyerPolicy
+{
+    use HandlesAuthorization;
+    public function viewAny(User $user, Buyer $buyer)
+    {
+        return $user->id === $buyer->id;
+    }
+    public function view(User $user, Buyer $buyer)
+    {
+        return $user->id === $buyer->id;
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Buyer::class => BuyerPolicy::class,
     ];
 
     /**
